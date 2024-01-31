6294ea39325488d7e04e5bfc4b7459ba0fc66992
Ronan Scotet <ronanscotet467@gmail.com>
2024-01-31 20:14:57 +0100

    fix error but stay one error

--- frontend/class/SoundManager.js
+++ frontend/class/SoundManager.js
@@ -1,14 +1,30 @@
 class SoundManager {
     constructor(game) {
         this.game = game;
-        this.audioContext = new AudioContext();
+        this.audioContext = null;
+        this.gainNode = null;
         this.init();
     }
 
     init() {
-        this.gainNode = this.audioContext.createGain();
-        this.gainNode.connect(this.audioContext.destination);
-        this.gainNode.gain.value = 0.5;
+        this.createAudioContext();
+    }
+
+    createAudioContext() {
+        if (!this.audioContext) {
+            this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+            this.gainNode = this.audioContext.createGain();
+            this.gainNode.connect(this.audioContext.destination);
+            this.gainNode.gain.value = 0.5;
+        }
+    }
+
+    closeAudioContext() {
+        if (this.audioContext) {
+            this.audioContext.close().catch(error => console.error('Erreur lors de la fermeture de AudioContext:', error));
+            this.audioContext = null;
+            this.gainNode = null;
+        }
     }
 
     async loadSound(url) {
@@ -17,8 +33,17 @@ class SoundManager {
             request.open('GET', url, true);
             request.responseType = 'arraybuffer';
 
-            request.onload = () => {
-                this.audioContext.decodeAudioData(request.response, resolve, reject);
+            request.onload = async () => {
+                try {
+                    if (this.audioContext.state === 'suspended') {
+                        await this.audioContext.resume();
+                    }
+
+                    const buffer = await this.audioContext.decodeAudioData(request.response);
+                    resolve(buffer);
+                } catch (error) {
+                    reject(error);
+                }
             };
 
             request.send();
@@ -26,7 +51,7 @@ class SoundManager {
     }
 
     async playSound(circle) {
-        if (this.game.soundEnabled) {
+        if (this.game.soundEnabled && this.audioContext) {
             try {
                 let soundUrl = '';
                 if (circle.classList.contains('red')) {
@@ -36,6 +61,11 @@ class SoundManager {
                 }
 
                 const buffer = await this.loadSound(soundUrl);
+
+                if (this.audioContext.state === 'suspended') {
+                    await this.audioContext.resume();
+                }
+
                 const source = this.audioContext.createBufferSource();
                 source.buffer = buffer;
                 source.connect(this.gainNode);
@@ -48,7 +78,16 @@ class SoundManager {
 
     toggleSound() {
         this.game.soundEnabled = !this.game.soundEnabled;
-        this.gainNode.gain.value = this.game.soundEnabled ? 0.5 : 0;
+
+        if (this.game.soundEnabled) {
+            this.createAudioContext();
+        } else {
+            this.closeAudioContext();
+        }
+
+        if (this.gainNode) {
+            this.gainNode.gain.value = this.game.soundEnabled ? 0.5 : 0;
+        }
     }
 }
 
