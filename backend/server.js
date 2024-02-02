9606fb94e7298a9d0b60b2eba0697e918c816789
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-02-02 14:01:20 +0100

    feat firebase

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const { initializeApp } = require('firebase/app');
+const { getFirestore, collection, getDocs } = require('firebase/firestore');
 
 const app = express();
 
-app.use(cors());
+const firebaseConfig = {
+  apiKey: "AIzaSyDQmbuusv0hvy1Mp6wJxnEBiBfv0ex5E30",
+  authDomain: "tape-taupe-32863.firebaseapp.com",
+  projectId: "tape-taupe-32863",
+  storageBucket: "tape-taupe-32863.appspot.com",
+  messagingSenderId: "174161939538",
+  appId: "1:174161939538:web:f53cae6964f799bca0e38b"
+};
 
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
 
-app.listen(8081, () => {
-    console.log("le serveur back tourne correctement")
-})
+if (db) {
+  console.log("Connexion à Firebase établie avec succès!");
+} else {
+  console.error("Erreur lors de la connexion à Firebase.");
+}
+
+app.use(cors({
+  origin: 'https://tape-taupe.vercel.app',
+  credentials: true,
+}));
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur votre API !');
+});
+
+app.get('/data', async (req, res) => {
+  try {
+    const dataRef = collection(db, 'users');
+    const snapshot = await getDocs(dataRef);
+    const data = snapshot.docs.map(doc => doc.data());
+    res.json(data);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données depuis Firestore', error);
+    res.status(500).send('Erreur serveur');
+  }
+});
+
+const port = process.env.PORT || 8081;
+
+app.listen(port, () => {
+  console.log(`Le serveur back tourne correctement sur le port ${port}`);
+});
