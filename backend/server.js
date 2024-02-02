89f8967292bb16c6c016033543863d0538efec3d
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-02-02 11:12:55 +0100

    feat vercel.json

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password:"",
-    database:"tape_taupe"
-})
 
 app.listen(8081, () => {
     console.log("le serveur back tourne correctement")
