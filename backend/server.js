9c4efc2230c17260732cd510e98c0aea2ca8bda5
Ronan Scotet <ronanscotet467@gmail.com>
2024-02-02 18:07:07 +0100

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, getDocs } = require('firebase/firestore');
+const { getAuth, signInWithEmailAndPassword } = require('firebase/auth');
 
 const app = express();
+app.use(cors());
+app.use(express.json()); // Ajout du middleware pour parser le JSON dans les requêtes
 
 const firebaseConfig = {
   apiKey: "AIzaSyDQmbuusv0hvy1Mp6wJxnEBiBfv0ex5E30",
@@ -16,6 +19,7 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
 
 if (db) {
   console.log("Connexion à Firebase établie avec succès!");
@@ -23,10 +27,20 @@ if (db) {
   console.error("Erreur lors de la connexion à Firebase.");
 }
 
-app.use(cors({
-  origin: 'https://tape-taupe.vercel.app',
-  credentials: true,
-}));
+// Middleware global pour les en-têtes CORS
+app.use((req, res, next) => {
+  const allowedOrigins = ['https://tape-taupe.vercel.app', 'http://localhost:5500'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 
 app.get('/', (req, res) => {
   res.send('Bienvenue sur votre API !');
@@ -41,6 +55,19 @@ app.get('/data', async (req, res) => {
   } catch (error) {
     console.error('Erreur lors de la récupération des données depuis Firestore', error);
     res.status(500).send('Erreur serveur');
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    res.status(200).json({ uid: user.uid, email: user.email });
+  } catch (error) {
+    console.error('Erreur lors de la connexion utilisateur', error);
+    res.status(401).send('Identifiants invalides');
   }
 });
 
